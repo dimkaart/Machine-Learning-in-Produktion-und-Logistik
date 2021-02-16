986d69976f8c4f7ba2ab71f11bbfd9d84cae8a74
Dmitri <56168624+dimkaart@users.noreply.github.com>
2021-02-16 23:42:54 +0100

    Add files via upload

---
+++ Pipeline_detect.py
@@ -0,0 +1,197 @@
+# Define environment
+import os
+# comment out below line to enable tensorflow outputs
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+from absl import app, flags, logging
+from absl.flags import FLAGS, FlagValues
+import core.utils as utils
+from core.yolov4 import filter_boxes
+from core.functions import *
+from tensorflow.python.saved_model import tag_constants
+from PIL import Image
+import cv2
+import numpy as np
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+import Levenshtein as ls
+import shutil
+import itertools 
+import pandas as pd 
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
+
+# delete old detection results
+for filename in os.listdir("./Output/Crop/"):
+   os.remove("./Output/Crop/"+filename)
+for filename in os.listdir("./Output/Erkennung/"):
+   os.remove("./Output/Erkennung/"+filename)
+for folder in os.listdir("./Output_char/Crop/"):
+   shutil.rmtree("./Output_char/Crop/"+folder)
+for filename in os.listdir("./Output_char/Erkennung/"):
+   os.remove("./Output_char/Erkennung/"+filename)
+os.remove("./Output/Ergebnis.txt")
+
+# initialize solution .txt file
+ErgFile=open("./Output/Ergebnis.txt","w")
+
+# set up
+Output='./Output/Erkennung/'
+Output_char = './Output_char/Erkennung/'
+Model='./checkpoints/custom-416'
+Model_char = './checkpoints/char-416'
+
+# thresholds to accept found bounding box as license plate
+iOU=0.45
+score=0.50
+
+# flags for outer functions 
+flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
+flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
+
+# main function
+def main(_argv):
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+    cfg.YOLO.CLASSES = "./data/classes/custom.names"
+    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+    input_size = 416 #Resize image
+    
+    # load model
+    saved_model_loaded = tf.saved_model.load(Model, tags=[tag_constants.SERVING])
+
+    # loop through images in folder and run Yolov4 model on each
+    Pfad='./Input/'
+    input_names = []
+    input_vergleich = []
+    for image_path in os.listdir(Pfad):
+        input_vergleich += [image_path.split('.')[0]]
+        input_crop = image_path.split('_')[0]
+        input_crop = input_crop.split('.')[0]
+        input_names += [input_crop]
+        gesamt(Pfad = Pfad, image_path = image_path, save_model = saved_model_loaded, 
+            iOU = iOU, score = score, output = Output, pixel = 5) 
+        # functions.py applies YOLOv4 on the image to find the license plate and to crop along the result
+
+    cfg.YOLO.CLASSES = "./data/classes/char.names"
+    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+    # load model
+    saved_model_char = tf.saved_model.load(Model_char, tags=[tag_constants.SERVING])
+    Pfad = './Output/Crop/'
+    if not tune: # after tuning
+        Pfad = './Output/Crop//'
+        output_names = []
+        output_vergleich = []
+        for image_path in os.listdir(Pfad):
+            vergleich = image_path.split('.png')[0]
+            vergleich = vergleich.split('crop_1_')
+            output_vergleich += [vergleich[1]]
+            name_neu = image_path.split('_')[2]
+            name_neu = name_neu.split('.')[0]
+            output_names += [name_neu]
+            gesamt(Pfad = Pfad, image_path = image_path, save_model = saved_model_char, 
+                    iOU = iOU, score = 0.5, output=Output_char, license_plate = False, pixel = 2) # apply Yolov4 on the image to find characters 
+        distance_index = []
+        for i in input_names: # find images where no license plate has been found
+            if (i in output_names):
+                distance_index += [1]
+            else:
+                distance_index += [0]
+        list_0 = [i for i, value in enumerate(distance_index) if value == 0]
+        list_1 = [i for i, value in enumerate(distance_index) if value == 1]
+        distance = [None] * len(input_names) # distance list
+        for i in list_0:
+            distance[i] = len(input_names[i]) # if no license found => distance = number of character of true recognition
+        insgesamt = []         
+        path = './Output_char/Crop//'
+        for root, subdirectories, files in os.walk(path): # loop through every license plate and detected characters
+            for subdirectory in subdirectories:
+                result = ''
+                original = subdirectory.split('_')[2]
+                for file in os.listdir(os.path.join(root, subdirectory)): # do ocr on all characters
+                    result += ocr_neu(file, root, subdirectory) 
+                if result == '': # if no character has been recognized => use tesseract on whole license plate
+                    erg = ocr_tesseract(image_path = image_path, Pfad = Pfad)
+                    result = erg
+                insgesamt += [result]
+                if not final: # outside final challenge calculate distance
+                    index_name = input_names.index(original)
+                    distance[index_name] = ls.distance(result, original)
+        print('erkannte Kennzeichen:', insgesamt)
+        if not final:
+            if distance:
+                print('Distanz:', distance)
+                print('durchschnittliche Levenshtein-Distanz:', np.mean(distance))
+        print('Bilder in Input:', input_vergleich)
+        print('Bilder in Output:', output_vergleich)
+        for index in input_vergleich: # create txt output file
+            if not output_vergleich:
+                ErgFile.write(index + ":" + '\n')
+            elif (index in output_vergleich):
+                stelle = output_vergleich.index(index)
+                ErgFile.write(index + ":" + insgesamt[stelle] + '\n')
+            else: 
+                ErgFile.write(index + ":" + '\n')
+        ErgFile.close()
+    else: # tuning
+        Pfad = './Output/Crop//'
+        distance_tess = []
+        for image_path in os.listdir(Pfad): # do ocr on whole license plate
+            name_neu = image_path.split('_')[2]
+            name_neu = name_neu.split('.')[0]
+            res = ocr_tesseract(image_path = image_path, Pfad = Pfad)
+            distance_tess += [ls.distance(res, name_neu)]
+        print('tess_distance:', np.mean(distance_tess))
+        evaluation = []
+        params = {'psm': ['10', '13'], 'oem': ['1'], 
+            'pixel': [x for x in range(0, 4)], 'score': [round(x, 2) for x in np.arange(0.5, 0.76, 0.1)]} # possible parameters
+        keys = list(params)
+        for values in itertools.product(*map(params.get, keys)):
+            for folder in os.listdir("./Output_char/Crop/"):
+                shutil.rmtree("./Output_char/Crop/"+folder)
+            for filename in os.listdir("./Output_char/Erkennung/"):
+                os.remove("./Output_char/Erkennung/"+filename)
+            print('values:', values)
+            name_crop = []
+            for image_path in os.listdir(Pfad):
+                name_neu = image_path.split('_')[2]
+                name_neu = name_neu.split('.')[0]
+                name_crop += [name_neu]
+                gesamt(Pfad = Pfad, image_path = image_path, save_model = saved_model_char, 
+                        iOU = iOU, output=Output_char, license_plate = False, **dict(zip(keys, values))) 
+            insgesamt = []
+            distance = []
+            path = './Output_char/Crop//'
+            for root, subdirectories, files in os.walk(path):
+                for subdirectory in subdirectories:
+                    result = ''
+                    original = subdirectory.split('_')[2]
+                    print("original:", original)
+                    for file in os.listdir(os.path.join(root, subdirectory)):
+                        result += ocr_neu(file, root, subdirectory, tune = True, **dict(zip(keys, values)))
+                    insgesamt += [result]
+                    distance += [ls.distance(result, original)]
+            print('erkanntes Kennzeichen:', insgesamt)
+            print('Distanz:', distance)
+            evaluation.append([values[0], values[1], values[2], values[3], np.mean(distance)])
+        evaluation_df = pd.DataFrame(evaluation, columns = ['oem', 'psm', 'pixel', 'score', 'mean(distance)']) # create table as output
+        evaluation_sort = evaluation_df.sort_values(by =['mean(distance)'])
+        fig, ax =plt.subplots(figsize=(12,4))
+        ax.axis('tight')
+        ax.axis('off')
+        the_table = ax.table(cellText=evaluation_sort.values,colLabels=evaluation_sort.columns,loc='center')
+        pp = PdfPages("Evaluation.pdf")
+        pp.savefig(fig, bbox_inches='tight')
+        pp.close()     
+
+if __name__ == '__main__':
+    try:
+        tune = False
+        final = True
+        app.run(main)
+    except SystemExit:
+        pass
--- README.md
+++ README.md
@@ -1,1 +1,207 @@
-# Machine-Learning-in-Produktion-und-Logistik
+# Machine Learning in Produktion und Logistik
+
+<img src="/data/helpers/KennzeichenErkennung.png"  width="80%" >
+
+# Ziel
+Ziel dieses Projektes im Rahmen des Fachlabors "Machine Learning in Produktion und Logistik" an der TU Dortmund war es, eine Pipeline zur Kennzeichenerkennung zu entwickeln.
+Dabei sollten verschiedene Methoden getestet werden und am Ende die beste ausgewählt werden.
+- Die Ideen wurden in der Gruppe erarbeitet und den Kursteilnehmern präsentiert
+- Vorworfen wurden beispielsweise
+  - Reine Edge Detection (mittels OpenCV)
+  - versch. Aufbereitungsmethoden
+  - etc.
+- finale Pipeline:
+  - YOLOv4 zur Kennzeichenerkennung
+  - YOLOv4 zur Buchstabenerkennung und segmentierung
+  - PyTesseract für Buchstabenübersetzung 
+  - Image PreProcessing in **allen** Schritten
+  
+
+## Die Idee hinter der Pipeline
+![](/data/helpers/Pipeline.png)
+![](/data/helpers/Folie4.JPG)
+
+## Pseudocode
+- Initialiseren
+  - Ergebnisdatei öffnen
+  - YOLOv4 Modelle laden
+- Für jedes Bild in Input Ordner
+  - Suche Kennzeichen mittels YOLOv4 Modell
+  - Markiere Kennzeichen (iou Schwellwert=0.5)
+  - Schneide das Bild auf Kennzeichen zu und lege entsprechend ab
+- Für jedes Bild in "Zugeschnittenen Kennzeichen" Ordner
+  - prüfe ob Ausrichtung des Kennzeichens (drehen) notwendig
+    - wenn ja, durchführen. Sonst Bild beibehalten
+  - Suche nach Klasse: "Buchstabe"
+  - Markiere Buchstaben und vergleiche Proportionen
+  - Schneide auf Buchstaben zu und lege diese je Kennzeichen in einen Ordner
+- Für jedes Bild in "Buchstaben eines Kennzeichen" Ordner
+  - **PreProcessing**
+  - Durchlaufe OCR von PyTesseract
+    - falls nichts gefunden wurden wende OCR aufs ganze Kennzeichen an
+  - Füge String von links nach rechts zusammen
+- Ausgabe des String in Ergebnis.txt
+
+# Vorbereitung
+## Conda (Empfohlen)
+
+```bash
+# Tensorflow CPU
+conda env create -f conda-cpu.yml
+conda activate yolov4-cpu
+
+# Tensorflow GPU
+conda env create -f conda-gpu.yml
+conda activate yolov4-gpu
+```
+### Pip (Alternativ)
+```bash
+# TensorFlow CPU
+pip install -r requirements.txt
+
+# TensorFlow GPU
+pip install -r requirements-gpu.txt
+```
+
+### Nvidia Driver (Für GPU, wenn du nicht Conda Environment nutzt und CUDA installiert hast)
+Make sure to use CUDA Toolkit version 10.1 as it is the proper version for the TensorFlow version used in this repository.
+https://developer.nvidia.com/cuda-10.1-download-archive-update2
+
+## Custom Weights Kennzeichen implementieren
+Wir haben für das Modell verschiedene YOLOv4 Modelle trainiert und mittels 10-Fold CrossValidation vergleichen.
+Ein Auszug aus den Ergebnissen nachfolgend:
+![](/data/helpers/CrossValid_final.png)
+
+#### Zusätzlich galt es den Loss zu analysieren.
+<img src="/data/helpers/Test%201%200-1500.jpg"  width="50%" >
+
+Zu sehen ist, dass der iou Loss immer weiter abnimmt. Der Unterschied zwischen "Ground Truth" und Prediticion immer geringer wird.
+Das Training muss rechtzeitig gestoppt werden, um ein Übertraining zu verhindern. Wir nehmen die Weights nach 1.500 Iterationen, da diese sowohl im Testdatensatz innerhalb des Trainings, als auch manueller Kontrolle die besten Ergebnisse hervorrufen.
+
+
+<img src="/data/helpers/iou%20loss.png"  width="50%" >
+
+Aufgrund von begrenztem Speicher im GitHub sind die von uns trainierten Weights extern zu beziehen.
+
+## Custom Weights Character implementieren
+
+Iterationen | mAP [%] | IoU [%]
+--- | --- | ---
+1000 | 99.69 | 76.79
+1100 | 99.60 | 78.82
+1200 | 99.70 | 80.29 
+1300 | 99.46 | 81.87
+1400 | 95.75 | 64.67
+1500 | 99.23 | 76.21
+1600 | 99.76 | 80.38
+1700 | 99.71 | 81.78
+1800 | 99.89 | 82.81
+1900 | 99.80 | 83.90
+2000 | 99.92 | 83.27
+2100 | 99.78 | 83.06
+2200 | 99.64 | 82.71
+2300 | 99.67 | 83.04
+2400 | 99.71 | 83.34
+2500 | 99.54 | 79.82
+
+## Ergebnisse des Tunings
+
+oem | psm | pixel | score | Ø-Levenshtein-Distanz
+--- |--- |---|---|---
+13 | 1 | 2 | 0.5 | 2.648
+13 | 1 | 2 | 0.6 | 2.648
+13 | 1 | 2 | 0.7 | 2.653
+13 | 1 | 1 | 0.6 | 2.775
+13 | 1 | 1 | 0.7 | 2.780
+13 | 1 | 1 | 0.5 | 2.780
+13 | 1 | 3 | 0.6 | 2.784
+13 | 1 | 3 | 0.7 | 2.784
+13 | 1 | 3 | 0.5 | 2.784
+10 | 1 | 2 | 0.5 | 2.872
+10 | 1 | 2 | 0.6 | 2.872
+10 | 1 | 2 | 0.7 | 2.874
+10 | 1 | 1 | 0.6 | 3.013
+10 | 1 | 1 | 0.5 | 3.015
+10 | 1 | 1 | 0.7 | 3.016
+10 | 1 | 3 | 0.5 | 3.058
+10 | 1 | 3 | 0.6 | 3.058
+10 | 1 | 3 | 0.7 | 3.060
+13 | 1 | 0 | 0.6 | 3.463
+13 | 1 | 0 | 0.5 | 3.465
+13 | 1 | 0 | 0.7 | 3.465
+10 | 1 | 0 | 0.6 | 3.584
+10 | 1 | 0 | 0.5 | 3.585
+10 | 1 | 0 | 0.7 | 3.588
+
+## Zum Download der Weights bitte die entsprechenden Depotlinks nutzen.
+### Kennzeichen Erkennung 
+- **https://depot.tu-dortmund.de/e8faf** 
+- Die Datei bitte im ./data/ Ordner als "custom.weights" -Datei ablegen.
+
+### Character Segmentation:
+- **https://depot.tu-dortmund.de/hvw7v**
+- Die Datei in ./data/ als "char.weights" ablegen.
+
+## YOLOv4 Tensorflow (tf, .pb model) einstellen
+To implement YOLOv4 using TensorFlow, first we convert the .weights into the corresponding TensorFlow model files.
+```bash
+# Convert darknet weights to tensorflow
+
+# Kennzeichen
+python save_model.py --weights ./data/custom.weights --output ./checkpoints/custom-416 --input_size 416 --model yolov4 
+
+# Character Segmentation
+python save_model.py --weights ./data/char.weights --output ./checkpoints/char-416 --input_size 416 --model yolov4 --nolicense
+```
+
+## Tesseract Pfad anpassen
+In functions.py in Zeile 14 den Pfad zu "tesseract.exe" anpassen.
+![](/data/helpers/TesseractPfad.png)
+
+# Pipeline anwenden
+## Bilder in Input Ordner legen
+Die zu erkennenden Bilder von Fahrzeugen mit Kennzeichen bitte in den ./Input/ Ordner legen.
+![](/data/helpers/Input.png)
+
+## Einstellungsmöglichkeiten Pipeline
+In Zeile 194 & 194 können die Parameter final & tune eingestellt werden. Dabei wird mit tune = True das Tuning durchgeführt und final = True berechnet keine Levenshtein Distanz.
+
+## Pipeline ausführen
+```
+# cmd öffnen
+# prüfen ob conda env yolov4-cpu/gpu aktiv ist (s. oben)
+
+# Pipeline starten:
+python Pipeline_detect.py
+```
+
+## Die Ergebnisse befinden sich im ./Output/ Ordner
+Innerhalb des Ordners befinden sich:
+- Crop, die zugeschnittenen Fotos auf das Kennzeichen
+- Erkennung, Bounding Box und Genauigkeit auf Fahrzeug
+- Textdatei mit Ergebnissen
+![](/data/helpers/Output.png)
+
+Es wird zusätzlich eine Ergebnis-Datei erstellt, in der Bildname und erkanntes Kennzeichen bzw. die Buchstaben ausgegeben werden.
+![](/data/helpers/Ergebnis.png)
+
+## Übersicht der Preprocessing Schritte:
+![](/data/helpers/Screenshots/uebersicht.png)
+- Nutzung der Preprocessing Möglichkeiten von OpenCV
+![](/data/helpers/Screenshots/loading.png)
+- Der Bildbereich innerhalb der Bounding-Box wird herausgeschnitten und geladen
+![](/data/helpers/Screenshots/resizing.png)
+- Anschließend wird der Bildausschnitt vergrößert, da das Nummernschilder auf den meisten Bildern nur einen sehr kleinen Teilbereich darstellt. Zudem wird bei der OCR, eine bestimmte Pixelhöhe für die Extraktion benötigt
+![](/data/helpers/Screenshots/grayscaling.png)
+- Da für das Auslesen des Nummernschildes Farben vernachlässigt werden können, wird als nächstes das Bild in Graustufen konvertiert. Somit lässt sich zusätzlich Rechenleistung einsparen
+![](/data/helpers/Screenshots/gaussianblur.png)
+- Um des weiteren zusätzlich Rauschen und irrelevante Informationen zu entfernen, wird die Gaußche-Unschärfetechnik verwendet. Wichtig ist dabei zu beachten, dass je höher der Einstellungswert, desto weniger Rauschen entsteht, jedoch auch gleichzeitig mehr Bildinformation verloren wird
+![](/data/helpers/Screenshots/thresholding.png)
+- Damit die Zeichen auf dem Nummernschild herausstechen, muss des weiteren mit Hilfe des Schwellenwertverfahrens nach der Otsu-Methode der Vordergrund vom Hintergrund getrennt werden, indem der Hintergrund umgekehrt wird
+![](/data/helpers/Screenshots/dilatation.png)
+- Anschließend wird die Dilatationstechnik implementiert, um den weißen Bereich des Bildes zu vergrößern, sodass die weiße Kontur jedes Zeichens verstärkt wird
+
+# Verworfene Ideen auf dem Weg zur finalen Pipeline
+![](/data/helpers/Folie3.JPG)
+
+
